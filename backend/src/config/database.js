3a04d45c2e9e6b0a8784623f952dbecc1ca4cc1d
Abhishek Patel <apate320@charlotte.edu>
2026-04-01 16:56:49 -0400

    Added Migration Script

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,24 +1,39 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
+const dataUrl = process.env.DB_DIALECT;
 // Create Sequelize instance (this connects to PostgreSQL)
-const sequelize = new Sequelize(
-    process.env.DB_NAME,      // Database name: motivatemate_dev
-    process.env.DB_USER,      // Username: motivatemate_user
-    process.env.DB_PASSWORD,  // Your password
-    {
-        host: process.env.DB_HOST,      // localhost
-        port: process.env.DB_PORT,      // 5432
-        dialect: process.env.DB_DIALECT, // postgres
-        logging: process.env.NODE_ENV === 'development' ? console.log : false,
-        pool: {
-            max: 5,      // Maximum number of connections
-            min: 0,      // Minimum number of connections
-            acquire: 30000, // Max time (ms) to get connection
-            idle: 10000     // Time (ms) before idle connection closes
+let sequelize;
+
+if (dataUrl) {
+    console.log("Using DATABASE_URL connection");
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        logging: false,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
         }
-    }
-);
+    });
+}
+
+else {
+    // Local development: Use individual parameters from .env
+    console.log('Using local database connection');
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            dialect: 'postgres',
+            logging: console.log
+        }
+    );
+}
 
 // Test the connection
 const testConnection = async () => {
